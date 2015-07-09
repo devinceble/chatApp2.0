240cf8f620d10da9940a182860ae194b2358a0a4
Leivince John D. Marte <devinceble@gmail.com>
2015-07-09 18:56:44 +0800

    Initialize Project

---
+++ client/templates/chat-template.js
@@ -0,0 +1,31 @@
+Template.appChat.helpers({
+	messages: function () {
+		return Messages.find({}, 
+			{sort: 
+				{
+					createdAt: -1
+				}, 
+			limit: 5
+		});
+	}
+});
+
+Template.appChat.events({
+	"click .btnDelete": function (event) {
+		Messages.remove(this._id);
+		return false;
+	},
+	"keyup .txtText": function (event) {
+		if(event.keyCode == 13){
+			Messages.insert({
+				name: $(".txtName").val(),
+				text: $(".txtText").val(),
+				createdAt: new Date()
+			});
+			
+			$(".txtText").val("");
+			$(".txtText").focus();
+		}
+		return false;
+	}
+});
