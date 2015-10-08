a182e94d528fb89c51f35c8bf5dcf4d4e63663a1
Leivince John D. Marte <devinceble@gmail.com>
2015-10-08 15:43:38 +0800

    Fixed Backbutton Cordova

--- client/templates/mainEventsAndHelpers.js
+++ client/templates/mainEventsAndHelpers.js
@@ -4,11 +4,24 @@ Template.registerHelper('_', function() {
 	return _;
 });
 
+if(Meteor.isCordova){
+	document.addEventListener("backbutton", onBackButtonDown, false);
+
+	function onBackButtonDown(event) {
+	  event.preventDefault();
+	  event.stopPropagation();
+		var route_name = Router.current().route.getName();
+
+		if(route_name == "Home"){
+			navigator.app.exitApp();
+		}else{
+			navigator.app.backHistory();
+		}
+	}
+}
+
 Template.body.events({
 	"click #btnLogout": function(){
-		if(Meteor.isCordova){
-			navigator.app.exitApp();
-		}
 		Meteor.logout();
 	}
 });
